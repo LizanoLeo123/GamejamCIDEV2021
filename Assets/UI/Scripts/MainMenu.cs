fc50a0e6cb1c26d058324cdba932fd6aff6e574d
Kevin Solano <ksolanoj@icloud.com>
2021-06-26 23:34:08 -0600

    Animation

--- Assets/UI/Scripts/MainMenu.cs
+++ Assets/UI/Scripts/MainMenu.cs
@@ -5,14 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+    public float transitionTime = 1f;
+    public bool isLevel = false;
+
+
+    private void Awake()
+    {
+        if (isLevel) StartCoroutine(LoadLevelText());
+    }
     public void PlayButtonPressed()
     {
-        SceneManager.LoadScene("Level-1");        
+        StartCoroutine(LoadLevel("Level-1"));        
     }
 
     public void AboutButtonPressed()
     {
-        SceneManager.LoadScene("About");
+        
     }
 
+    IEnumerator LoadLevel(string levelName)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelName);
+    }
+
+    IEnumerator LoadLevelText()
+    {
+        transition.SetBool("IsLevel", true);
+        yield return new WaitForSeconds(transitionTime);
+        
+    }
 }
